5adf00d2bab6570a34715d77f290ac526607840b
Jonas Fluhrer <jonas.fluhrer@novatec-gmbh.de>
2022-10-28 15:19:56 +0200

    add persistence to msw and remove @mswjs/data

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,17 +1,11 @@
 import { rest } from "msw";
-import { db } from "./db";
+import { pokemonDb } from "./db";
 
 export const handlers = [
   rest.get("/api/v1/my-pokemon/:id", (req, res, ctx) => {
     const { id } = req.params;
 
-    const pokemon = db.pokemon.findFirst({
-      where: {
-        id: {
-          equals: id,
-        },
-      },
-    });
+    const pokemon = pokemonDb.getValue().find(p => p.id === id);
 
     if (!pokemon) {
       return res(ctx.status(400, `Pokemon with id '${name}' not found`));
@@ -22,54 +16,44 @@ export const handlers = [
   rest.post("/api/v1/my-pokemon/:id/level-up", async (req, res, ctx) => {
     const { id } = req.params;
 
-    const foundPokemon = db.pokemon.findFirst({
-      where: {
-        id: {
-          equals: id,
-        },
-      }
-    })
+    const foundPokemon = pokemonDb.getValue().find(p => p.id === id);
 
     if (!foundPokemon) {
       return res(ctx.status(400, `Pokemon with id '${id}' not found`));
     }
 
-    const newLevel = foundPokemon.level + 1;
-
-    const pokemon = db.pokemon.update({
-      where: {
-        id: {
-          equals: id,
-        },
-      },
-      data: {
-        level: newLevel,
-      },
+    pokemonDb.update((currentPokemon) => {
+      let pokemon1 = currentPokemon.find((p) => p.id === id);
+      pokemon1.level = pokemon1.level + 1;
+      currentPokemon.splice(currentPokemon.indexOf(pokemon1), 1, pokemon1);
+      return currentPokemon;
     });
 
-    return res(ctx.json(pokemon));
+    const updatedPokemon = pokemonDb.getValue().find(p => p.id === id);
+
+    return res(ctx.json(updatedPokemon));
   }),
 
   rest.post("/api/v1/my-pokemon/:id/set-nickname", async (req, res, ctx) => {
     const { id } = req.params;
 
-    const { nickname } = await req.json();
+    const foundPokemon = pokemonDb.getValue().find(p => p.id === id);
 
-    const pokemon = db.pokemon.update({
-      where: {
-        id: {
-          equals: id,
-        },
-      },
-      data: {
-        nickname: nickname,
-      },
-    });
-
-    if (!pokemon) {
+    if (!foundPokemon) {
       return res(ctx.status(400, `Pokemon with id '${id}' not found`));
     }
 
-    return res(ctx.json(pokemon));
+    const { nickname } = await req.json();
+
+    pokemonDb.update((currentPokemon) => {
+      let pokemon1 = currentPokemon.find((p) => p.id === id);
+      pokemon1.nickname = nickname;
+      currentPokemon.splice(currentPokemon.indexOf(pokemon1), 1, pokemon1);
+      return currentPokemon;
+    });
+
+    const updatedPokemon = pokemonDb.getValue().find(p => p.id === id);
+
+    return res(ctx.json(updatedPokemon));
   }),
 ];
